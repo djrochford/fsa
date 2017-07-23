75e5132eb298267373a0a9238ce92fa4bcb98fad
Damien Rochford <djrochford@gmail.com>
2017-07-23 16:32:23 -0400

    Refactor DFA as seperate class.

--- fsa.py
+++ fsa.py
@@ -1,5 +1,94 @@
 import itertools as it
-import unittest
+
+class DFA:
+    """A deterministic finite-state automaton class. Takes three parameters: a transition function, a start state 
+    and a set of accept states, in that order.
+
+    The transition function should be specified as a dictionary with tuple keys. 
+    These keys implicitly define the dfa's state-set and alphabet; the first elements of the tuples represent the
+    fsa's states, and the second elements are the symbols in the alphabet.
+
+    The fsa expects the symbols of the alphabet to be one character strings. States can be anything hashable. (Note that,
+    for reasons of hashability, you'll need to use frozensets, rather than sets, if you want to have sets as states.)
+    
+    The class will raise a ValueError exception on instantiation if any of the following are true:
+        1. the start state is not a member of the set of states inferred from the transition function;
+        2. the set of accept states is not a subset of the set of states inferred from the transition function;
+        3. the range of the transition function is not a subset of the set of states inferred from the transition function.
+        4. A member of the alphabet inferred from the transition function is not a one-character string.
+        5. The transition function is missing a case -- i.e., it is not the case that every pair of a state and a symbol
+        is in the domain of the transition function.
+    The exception message will specify which of these four conditions things triggered the exception, and which states/symbols
+    are the source of the problem."""
+
+    def __init__(self, transition_function, start_state, accept_states):
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.states, self.alphabet = self._extract_states_alphabet(transition_function)
+        self.transition_function = transition_function
+        self._well_defined()
+
+    def _extract_states_alphabet(self, transition_function):
+        [states_tuple, alphabet_tuple] = zip(*transition_function.keys())
+        return (set(states_tuple), set(alphabet_tuple))
+
+    def _well_defined(self):
+        self._good_start()
+        self._good_accept()
+        self._good_range()
+        self._good_alphabet()
+        self._good_transition()
+
+    def _good_start(self):
+        if self.start_state not in self.states:
+            raise ValueError("Start state not a member of the fsa's state set.")
+
+    def _good_accept(self):
+        non_state_accepts = self.accept_states - self.states
+        if non_state_accepts != set():
+            raise ValueError("Accept states {} not members of the fsa's state set.".format(*non_state_accepts))
+
+    def _good_range(self):
+        transition_range = set(self.transition_function.values())
+        if transition_range - self.states != set():
+            raise ValueError("States {} in the range of the transition function are not in the fsa's state set.".format(*non_state_range))
+
+    def _good_alphabet(self):
+        for symbol in self.alphabet:
+            if not (type(symbol) is str and len(symbol) == 1):
+                raise ValueError("Symbol {} in the alphabet is not a single character string".format(symbol))
+
+    def _good_transition(self):
+        missing_pairs = it.product(self.states, self.alphabet) - self.transition_function.keys()
+        if missing_pairs != set():
+            raise ValueError("Pairs {} missing from transition function domain.".format(*missing_pairs))
+
+    def __or__(self, dfa):
+        """Takes an dfa instance as a parameter. Returns an dfa instance that recognizes the language A union B, 
+        where A is the language recognized byself and B the language recognized by the input dfa."""
+        union_states = it.product(self.states, dfa.states)
+        union_alphabet = self.alphabet | dfa.alphabet
+        union_transition_function = {}
+        for pair in it.product(union_states, union_alphabet):
+            (state1, state2), symbol = pair
+            union_transition_function[pair] = (self.transition_function[(state1, symbol)], dfa.transition_function[(state2, symbol)])
+        union_start_state = (self.start_state, dfa.start_state)
+        union_accept_states = set(it.product(self.accept_states, dfa.accept_states)) | set(it.product(self.states, dfa.accept_states))
+        return DFA(union_transition_function, union_start_state, union_accept_states)
+
+    def accepts(self, string):
+        """Determines whether fsa accepts input string. Will raise a ValueError exception is the string contains
+        symbols that aren't in the dfa's alphabet."""
+        self._check_input(string)
+        current_state = self.start_state
+        for symbol in string:
+            current_state = self.transition_function[(current_state, symbol)]
+        return False if current_state not in self.accept_states else True
+
+    def _check_input(self, string):
+        non_alphabet_symbols = set(list(string)) - self.alphabet
+        if non_alphabet_symbols != set():
+            raise ValueError("Symbols {} not in fsa's alphabet".format(*non_alphabet_symbols))
 
 class FSA:
     """A finite-state automaton class. Takes three parameters: a transition function, a start state 
@@ -15,6 +104,10 @@ class FSA:
     can be either the fsa's state-set or the power-set of the fsa's state-set. 
     You can use either a state or its singleton set interchangeable as a value of the transition function;
     they are treated as equivalent.
+
+    You can also define epsilon-moves; use the empty string in place of an alphabet symbol in the transition function to do so.
+    Note that the empty string will not be inferred to be a member of the alphabet (and hence the checks below will work as
+    you would expect).
 
     The class will raise a ValueError exception on instantiation if any of the following are true:
         1. the start state is not a member of the set of states inferred from the transition function;
@@ -111,24 +204,27 @@ m2_transition = {
     ('q2', '1'): 'q2',
 }
 
-n1_transition = {
-    ('q1', '0'): 'q1',
-    ('q1', '1'): {'q1', 'q2'},
-    ('q2', '0'): {'q3'},
-    ('q2', '1'): 'q3',
-    ('q3', '0'): 'q4',
-    ('q3', '1'): 'q4',
-    ('q4', '0'): set(),
-    ('q4', '1'): set()
-}
+# n1_transition = {
+#     ('q1', '0'): 'q1',
+#     ('q1', '1'): {'q1', 'q2'},
+#     ('q2', '0'): {'q3'},
+#     ('q2', '1'): 'q3',
+#     ('q3', '0'): 'q4',
+#     ('q3', '1'): 'q4',
+#     ('q4', '0'): set(),
+#     ('q4', '1'): set()
+# }
 
-m_1 = FSA(m1_transition, 'q1', {'q2'})
-m_2 = FSA(m2_transition, 'q1', {'q1'})
+# n_2_transition = {}
+
+m_1 = DFA(m1_transition, 'q1', {'q2'})
+m_2 = DFA(m2_transition, 'q1', {'q1'})
 m_3 = m_1 | m_2
 
-n_1 = FSA(n1_transition, 'q1', {'q4'})
+# n_1 = FSA(n1_transition, 'q1', {'q4'})
+# n_2 = FSA()
 
 print(m_1.accepts('101'), m_1.accepts('1000'), m_2.accepts('101'), m_2.accepts('1000'))
 print(m_3.accepts('101'), m_3.accepts('1000'))
 
-print(n_1.accepts('00100'), n_1.accepts('0011'))
+# print(n_1.accepts('00100'), n_1.accepts('0011'))
