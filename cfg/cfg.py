a5088880637035a6521ca359cf36483da1a629bc
Damien Rochford <djrochford@gmail.com>
2017-09-23 10:36:07 -0400

    Add `chomsky_normalize` method to CFG class.

--- cfg/cfg.py
+++ cfg/cfg.py
@@ -5,7 +5,7 @@ class CFG:
         self.rules = rules
         self._check_rules()
         self.variables = set(self.rules.keys())
-        self.terminals = set.union(*map(self._parse_value, self.rules.values()))
+        self.terminals = set.union(*map(self._parse_values, self.rules.values()))
         self._check_terminals()
         self.start_variable = start_variable
         self._check_start()
@@ -13,17 +13,24 @@ class CFG:
     def _check_rules(self):
         if type(self.rules) != dict:
             raise ValueError("Rules parameter should be a dictionary.")
-        bad_variables = {x for x in self.rules.keys() if type(x) != str}
+        bad_variables = {x for x in self.rules if type(x) != str}
         self._error_message(
             bad_variables,
             "Variable {} is not a string.",
             "Variables {} are not strings."
         )
-        bad_values = {x for x in self.rules.values() if type(x) != str}
+        bad_values = {x for x in self.rules.values() if type(x) != set and type(x) != frozenset}
         self._error_message(
             bad_values,
-            "Value {} of rule dictionary is not a string.",
-            "Values {} of rules dictionary are not strings."
+            "Value {} of rules dictionary is not either a set or frozenset.",
+            "Values {} of rules dictionary are not either sets or frozensets."
+        )
+        members = set.union(*list(self.rules.values()))
+        bad_members = {x for x in members if type(x) != str}
+        self._error_message(
+            bad_members,
+            "Value member {} is not a string.",
+            "Value members {} are not strings."
         )
 
     def _check_terminals(self):
@@ -34,10 +41,9 @@ class CFG:
         if self.start_variable not in self.variables:
             raise ValueError("Start variable not in the CFG's variable set.")
 
-    def _parse_value(self, value):
-        values = value.split('|')
-        parse = lambda value: set(re.split('|'.join(self.variables), value)) - {'', '|'}
-        return set.union(*map(parse, values))
+    def _parse_values(self, substitution_set):
+        parse = lambda substitution: set(re.split('|'.join(self.variables), substitution)) - {''}
+        return set.union(*map(parse, substitution_set))
 
     def _error_message(self, bad_set, message_singular, message_plural):
         if bad_set != set():
@@ -47,30 +53,116 @@ class CFG:
             else:
                 raise ValueError(message_plural.format((", ").join(quoted_members)))
 
-    # def _well_defined(self):
-    #     self._good_variables(),
-    #     self._good_terminals(),
-    #     self._good_start()
+    def get_rules(self):
+        return self.rules.copy()
 
-    # def _good_variables(self):
-    #     bad_variables = { x for x in self.variables if not type(x) == str }
-    #     self._error_message(
-    #       bad_variables,
-    #       "Variable {} is not a string.",
-    #       "Variables {} are not strings."
-    #     )
+    def get_start_variable(self):
+        return self.start_variable
 
-    # def _good_terminals(self):
-    #     if terminals == set():
-    #         raise ValueError("Rules contain no terminals.")
-    #     bad_terminals = { x for x in terminals if not type(x) == str }
-    #     self._error_message(
-    #         bad_terminals
-    #         "Terminal {} is not a string."
-    #         "Terminals {} are not strings."
-    #     )
+    def get_variables(self):
+        return self.variables.copy()
 
-    # def _good_start(self):
-    #     if self.star_variable not in self.variables:
-  
-    #         raise ValueError("Start variable not in CFG's variable set.")
+    def get_terminals(self):
+        return self.terminals.copy()
+
+    def chomsky_normalize(self):
+
+        def get_new_variable(variable_set):
+            new_variable = 'V';
+            while new_variable in variable_set:
+                new_variable = new_variable + '`'
+            return new_variable
+        rule_set = {(v, s) for v in self.rules for s in self.rules[v]}
+        normalized_start = get_new_variable(self.variables)
+        rule_set.add((normalized_start, self.start_variable))
+        removed_rules = set()
+        rule_variables = {v for v, s in rule_set}
+        rule_substitutions = {s for v, s in rule_set}
+        while rule_substitutions & (rule_variables | {'€'}) != set():
+            for rule in list(rule_set):
+                variable, substitution = rule
+                if substitution in rule_variables | {'€'}:
+                    rule_set.remove(rule)
+                    removed_rules.add(rule)
+                    for other_rule in list(rule_set):
+                        other_variable, other_substitution = other_rule
+                        if substitution == '€':
+                            if variable in other_substitution:
+                                for match in re.finditer(variable, other_substitution):
+                                    new_substitution = other_substitution[:match.start()] + other_substitution[match.end()+1:]
+                                    if new_substitution == '':
+                                        new_substitution = '€'
+                                    new_rule = (other_variable, new_substitution)
+                                    if not (new_rule in removed_rules):
+                                        rule_set.add(new_rule)
+                        else:
+                            if other_variable == substitution:
+                                new_rule = (variable, other_substitution)
+                                if not (new_rule in removed_rules):
+                                    rule_set.add(new_rule)
+            rule_substitutions = {s for v, s in rule_set}
+            rule_variables = {v for v, s in rule_set}
+
+        def parse_substitution(string):
+            words = {v for v, s in rule_set} | self.terminals
+            word_candidate = ''
+            parsed = []
+            for char in string:
+                word_candidate += char
+                if char in words:
+                    parsed.append(word_candidate)
+                    word_candidate = ''
+            return parsed
+
+        three_word_rules = {rule for rule in rule_set if len(parse_substitution(rule[1])) >=3}
+        while three_word_rules != set():
+            for rule in three_word_rules:
+                variable, substitution = rule
+                rule_set.remove(rule)
+                parsed_substitution = parse_substitution(substitution)
+                last_variable = variable
+                for word in parsed_substitution[:-2]:
+                    new_variable = get_new_variable({v for v, s in rule_set | {(variable, substitution)}})
+                    new_rule = (last_variable, word + new_variable)
+                    rule_set.add(new_rule)
+                    last_variable = new_variable
+                final_rule = (last_variable, ''.join(parsed_substitution[-2:]))
+                rule_set.add(final_rule)
+            three_word_rules = {rule for rule in rule_set if len(parse_substitution(rule[1])) >= 3}
+        
+        def get_bad_remaining_rules():
+            bad_rules = set()
+            for rule in rule_set:
+                parsed_substitution = parse_substitution(rule[1])
+                if len(parsed_substitution) == 2 and (self.terminals & set(parsed_substitution) != set()):
+                    bad_rules.add(rule)
+            return bad_rules
+
+        bad_remaining_rules = get_bad_remaining_rules()
+        while bad_remaining_rules != set():
+            print(rule_set)
+            print(bad_remaining_rules)
+            print("+++++++++++++++++++")
+            for rule in bad_remaining_rules:
+                variable, substitution = rule
+                parsed_substitution = parse_substitution(substitution)
+                for word in set(parsed_substitution):
+                    if word in self.terminals:
+                        new_variable = get_new_variable({v for v, s in rule_set})
+                        rule_set.add((new_variable, word))
+                        for other_rule in get_bad_remaining_rules():
+                            other_variable, other_substitution = other_rule
+                            if word in other_substitution:
+                                rule_set.remove(other_rule)
+                                new_substitution = other_substitution.replace(word, new_variable)
+                                rule_set.add((other_variable, new_substitution))
+            bad_remaining_rules = get_bad_remaining_rules()
+
+        normalized_rules = {}
+        for rule in rule_set:
+            variable, substitution = rule
+            if variable in normalized_rules:
+                normalized_rules[variable].add(substitution)
+            else:
+                normalized_rules[variable] = {substitution}
+        return CFG(normalized_rules, normalized_start)
--- cfg/tests.py
+++ cfg/tests.py
@@ -5,32 +5,32 @@ class TestCFG(unittest.TestCase):
 
     def setUp(self):
         self.rules1 = {
-            'A': '0A1|B',
-            'B': '#'
+            'A': {'0A1', 'B'},
+            'B': {'#'}
         }
 
         rules2 = {
-            '<SENTENCE>': '<NOUN-PHRASE> <VERB-PHRASE>',
-            '<NOUN-PHRASE>': '<CMPLX-NOUN>|<CMPLX-NOUN> <PREP-PHRASE>',
-            '<VERB-PHRASE>': '<CMPLX-VERB>|<CMPLX-VERB> <PREP-PHRASE>',
-            '<PREP-PHRASE>': '<PREP> <CMPLX-NOUN>',
-            '<CMPLX-NOUN>': '<ARTICLE> <NOUN>',
-            '<CMPLX-VERB>': '<VERB>|<VERB> <NOUN-PHRASE>',
-            '<ARTICLE>': 'a',
-            '<ARTICLE>': 'the',
-            '<NOUN>': 'boy|girl|flower',
-            '<VERB>': 'touches|likes|sees',
-            '<PREP>': 'with'
+            '<SENTENCE>': {'<NOUN-PHRASE> <VERB-PHRASE>'},
+            '<NOUN-PHRASE>': {'<CMPLX-NOUN>', '<CMPLX-NOUN> <PREP-PHRASE>'},
+            '<VERB-PHRASE>': {'<CMPLX-VERB>', '<CMPLX-VERB> <PREP-PHRASE>'},
+            '<PREP-PHRASE>': {'<PREP> <CMPLX-NOUN>'},
+            '<CMPLX-NOUN>': {'<ARTICLE> <NOUN>'},
+            '<CMPLX-VERB>': {'<VERB>', '<VERB> <NOUN-PHRASE>'},
+            '<ARTICLE>': {'a'},
+            '<ARTICLE>': {'the'},
+            '<NOUN>': {'boy', 'girl', 'flower'},
+            '<VERB>': {'touches', 'likes', 'sees'},
+            '<PREP>': {'with'}
         }
 
         rules3 = {
-            'S': "aSb|SS|€"
+            'S': {'aSb','SS', '€'}
         }
 
         rules4 = {
-            '<EXPR>': '<EXPR>+<TERM>|<TERM>',
-            '<TERM>': '<TERM>*<FACTOR>|<FACTOR>',
-            '<FACTOR>': '(<EXPR>)|a'
+            '<EXPR>': {'<EXPR>+<TERM>', '<TERM>'},
+            '<TERM>': {'<TERM>*<FACTOR>', '<FACTOR>'},
+            '<FACTOR>': {'(<EXPR>)', 'a'}
         }
 
         self.g1 = CFG(self.rules1, 'A')
@@ -41,20 +41,23 @@ class TestCFG(unittest.TestCase):
     def test_instantiation(self):
         not_dict_msg = "Rules parameter should be a dictionary."
         bad_variables_msg = "Variable '0' is not a string."
-        bad_values_msg = "Values ('101', 'True'|'True', '101') of rules dictionary are not strings."
+        bad_values_msg = "Values ('#', '01'|'01', '#') of rules dictionary are not either sets or frozensets."
         bad_terminals_msg = "There are no terminals in the rule dictionary values." 
         bad_start_msg = "Start variable not in the CFG's variable set."
+        bad_value_members_msg = "Value member '2' is not a string."
 
         rules1 = ['dog', 'cat']
         rules2 = self.rules1.copy()
         rules2[0] = 'B'
         rules3 = self.rules1.copy()
-        rules3['C'] = 101
-        rules3['D'] = True
+        rules3['C'] = '#'
+        rules3['D'] = '01'
         rules4 = {
-            'A': 'A|B',
-            'B': 'A'
+            'A': {'A', 'B'},
+            'B': {'A'}
         }
+        rules5 = self.rules1.copy()
+        rules5['C'] = {2}
 
         with self.assertRaisesRegex(ValueError, not_dict_msg):
             CFG(rules1, 'dog')
